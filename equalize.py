68c7261d7191a9004c2e5b565e5a9324cd15c9c0
jasonyuan <jasonyuan97@outlook.com>
2019-03-31 15:24:57 -0500

    added a temporary equalize function for w<=5

--- equalize.py
+++ equalize.py
@@ -9,24 +9,54 @@ def equalize(l, n):
         return [1]*l
     if (l==2):
         return random.choice([[1, 0], [0, 1]])
-    if (n==1 and l%2==0):
-        return random.choice([equalize(l-1, 1)+[0], [0]+equalize(l-1, 1)])
-
-    if (l%2 != 0): # l is odd, l >= 3
-        if (n%2 != 0): # n is odd
-            return equalize(l//2, n//2)+[1]+equalize(l//2, n//2)
-        else: # n is even
-            return equalize(l//2, n//2)+[0]+equalize(l//2, n//2)
-    else: # l is even, l >= 4
-        if (n<=l/2): 
-            if (n%2==0): # n is even
-                return equalize(l//2, n//2)+equalize(l//2, n//2)
-            else: # n is odd
-                # method 1
-                l1 = equalize(l//2, (n-1)//2)
-                l2 = equalize(l//2, (n+1)//2)
-                return random.choice([l1+l2, l2+l1])
-                # method 2
-                
-        else:
-            return [1-x for x in equalize(l, l-n)]
+    if (l==3):
+        if (n==1):
+            return [0,1,0]
+        if (n==2):
+            return [1,0,1]
+    if (l==4):
+        if (n==1):
+            return random.choice([[0,1,0,0], [0,0,1,0]])
+        if (n==2):
+            return random.choice([[0,1,0,1], [1,0,1,0]])
+        if (n==3):
+            return random.choice([[1,1,0,1], [1,0,1,1]])
+    if (l==5):
+        if (n==1):
+            return [0,0,1,0,0]
+        if (n==2):
+            return [0,1,0,1,0]
+        if (n==3):
+            return [1,0,1,0,1]
+        if (n==4):
+            return [1,1,0,1,1]
+        
+# def equalize(l, n):
+#     if (n==0):
+#         return [0]*l
+#     if (l==n):
+#         return [1]*l
+#     if (l==2):
+#         return random.choice([[1, 0], [0, 1]])
+#     if (n==1 and l%2==0):
+#         return random.choice([equalize(l-1, 1)+[0], [0]+equalize(l-1, 1)])
+# 
+#     if (l%2 != 0): # l is odd, l >= 3
+#         if (n%2 != 0): # n is odd
+#             return equalize(l//2, n//2)+[1]+equalize(l//2, n//2)
+#         else: # n is even
+#             return equalize(l//2, n//2)+[0]+equalize(l//2, n//2)
+#     else: # l is even, l >= 4
+#         if (n<=l/2): 
+#             if (n%2==0): # n is even
+#                 return random.choice([equalize(l-1, n)+[0], [0]+equalize(l-1, n)])
+#                 # return equalize(l//2, n//2)+equalize(l//2, n//2)
+#             else: # n is odd
+#                 # method 1
+#                 l1 = equalize(l//2, (n-1)//2)
+#                 l2 = equalize(l//2, (n+1)//2)
+#                 return random.choice([l1+l2, l2+l1])
+#                 # method 2
+#                 
+#         else:
+#             return [1-x for x in equalize(l, l-n)]
