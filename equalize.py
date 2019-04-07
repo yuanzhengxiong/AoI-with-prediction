c3fb8287138ed8e6fac590eec1b7cd839b1cb681
jasonyuan <jaosnyuan97@outlook.com>
2019-04-07 16:50:24 -0500

    accelerate equalize func

--- equalize.py
+++ equalize.py
@@ -4,6 +4,12 @@ from funcs import get_reverse
 import random
 
 def equalize(l, n):
+    if n==0:
+        return [0]*l
+    if l==n:
+        return [1]*l
+    if n > l/2:
+        return [1-x for x in equalize(l, l-n)]
     b = (l-n)%(n+1) 
     k = (l-n)//(n+1)
     zero_nums = [k]*(n+1-b)+[k+1]*b         
