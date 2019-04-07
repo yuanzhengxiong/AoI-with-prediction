8cc6215c9ea8d7af30e61632bc1a1455ee3f409b
jasonyuan <jaosnyuan97@outlook.com>
2019-04-07 18:54:22 -0500

    use array instead of list

--- aoi.py
+++ aoi.py
@@ -2,15 +2,10 @@ from __future__ import division
 import numpy as np
 
 class AoI:
-    def __init__(self, arrival_seq, init_age=0):
-        self.cur = init_age
-        self.seq = [init_age]
-        for arrival in arrival_seq:
-            self.op(arrival)
-        self.seq = np.array(self.seq) 
+    def __init__(self, arrival_seq):
+        self.seq = np.zeros((arrival_seq.size+1,), dtype=int)
+        for i, arrival in enumerate(arrival_seq):
+            if arrival==0:
+                self.seq[i+1] = self.seq[i]+1
         self.max = self.seq.max()
         self.avg = self.seq.mean()
-    
-    def op(self, arrival): # AoI operator
-        self.cur = self.cur+1 if arrival==0 else 0
-        self.seq.append(self.cur) 
