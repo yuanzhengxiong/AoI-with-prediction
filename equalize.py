d13edb912ce8a6c9b40f604d8b71b832e08ca5b0
jasonyuan <jaosnyuan97@outlook.com>
2019-04-09 00:05:44 -0500

    Revert "use array instead of list"
    This reverts commit 738f27bbf5a64b2fb49085b1ba824e9962a44219.

--- equalize.py
+++ equalize.py
@@ -1,23 +1,20 @@
 from __future__ import division
-#from random import shuffle
+from random import shuffle
 from funcs import get_reverse
 import random
-import numpy as np
 
 def equalize(l, n):
     if n==0:
-        return np.zeros((l,), dtype=int)
+        return [0]*l
     if l==n:
-        return np.ones((l,), dtype=int)
+        return [1]*l
     if n > l/2:
-        return 1-equalize(l, l-n)
+        return [1-x for x in equalize(l, l-n)]
     b = (l-n)%(n+1) 
     k = (l-n)//(n+1)
-    zero_nums = np.concatenate((np.zeros((n+1-b,), dtype=int)+k,np.zeros((b,), dtype=int)+(k+1)))
-    np.random.shuffle(zero_nums)
-    ret = np.zeros((l,), dtype=int)
-    t = 0
-    for x in zero_nums[:-1]:
-        ret[t+x] = 1
-        t = t+x+1
-    return ret
+    zero_nums = [k]*(n+1-b)+[k+1]*b         
+    shuffle(zero_nums)
+    ret = []
+    for x in zero_nums:
+        ret = ret+[0]*x+[1] 
+    return ret[:-1]
