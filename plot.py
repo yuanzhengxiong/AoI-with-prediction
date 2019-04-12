3944adf028ce6ef5e2e86d328072054f4a1b07b1
jasonyuan <jasonyuan97@outlook.com>
2019-04-12 16:39:53 -0500

    nothing

--- plot.py
+++ plot.py
@@ -1,5 +1,7 @@
 from __future__ import division
 from mpl_toolkits import mplot3d
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
@@ -26,8 +28,6 @@ def plot_3d(df_ps, age_type):
         ax.set_title('p='+str((i+1)/10))
 
 def plot_s_given_pw(df, p, w, age_type, ax, img_path, writer):
-    #base_avg_age = df[df.w==1]['avg_age'].values[0]
-    #base_max_age = df[df.w==1]['max_age'].values[0]
     age_type = age_type+'_age'
     base_age = df[df.w==1][age_type].values[0]
     df_w = df[df.w==w]
@@ -50,31 +50,6 @@ def plot_s_given_pw(df, p, w, age_type, ax, img_path, writer):
                  'gain': '{:f}'.format(gain)}
     writer.writerow(data_dict)
 
-    # if (age_type =='avg'):
-    #     avgs = df_w['avg_age'].values
-    #     ax.plot(ss, avgs)
-    #     ax.plot([1,w], [base_avg_age, base_avg_age])
-    #     ax.scatter(ss, avgs)
-    #     ax.set_ylabel('avg_age')
-    #     best_row = df.iloc[df_w['avg_age'].idxmin()]
-    #     s = best_row['s']
-    #     best_avg_age = best_row['avg_age']
-    #     gain = best_avg_age/base_avg_age
-    #     data_dict = {'p': '{:.2f}'.format(p), 
-    #                  'w': '{:03d}'.format(int(w)),
-    #                  's': '{:03d}'.format(int(s)),
-    #                  'base': '{:f}'.format(base_avg_age), 
-    #                  'best': '{:f}'.format(best_avg_age), 
-    #                  'gain': '{:f}'.format(gain)}
-    #     writer.writerow(data_dict)
-
-    # else:
-    #     maxs = df_w['max_age'].values
-    #     ax.plot(ss, maxs)
-    #     ax.plot([1,w], [base_max_age, base_max_age])
-    #     ax.scatter(ss, maxs)
-    #     ax.set_ylabel('max_age')
-
     ax.set_xlabel('s')
     title = 'p={0:.2f}, w={1:03d}'.format(p, w)
     ax.set_title(title)
@@ -84,32 +59,35 @@ def plot_s_given_pw(df, p, w, age_type, ax, img_path, writer):
 def main():
     avg_num = int(1e4)
     time_range = 300
-    arrival_type = 'Bernoulli'
+    arrival_type = 'Markovian'
     policy = 'equal_spreading' 
-    w_range = [1, time_range//10]
-    age_type = 'avg'
+    w_range = [1, 50]
+    age_type = 'max'
 
-    img_path = './imgs/av_{0}_tr_{1}_ar_{2}/'.format(avg_num, time_range, arrival_type)
+    img_path = '/work/LAS/jialiu-lab/zyuan/AoI/imgs/av_{0}_tr_{1}_ar_{2}/'.format(avg_num, time_range, arrival_type)
     if not os.path.isdir(img_path):
         os.mkdir(img_path)
     img_path = img_path+age_type+'/'
     if not os.path.isdir(img_path):
         os.mkdir(img_path)
 
-    csv_file = open(img_path+'{:s}_analysis.csv'.format(age_type), mode='w')
+    csv_file = open(img_path+'{0:s}_wr_{1:03d}-{2:03d}_analysis.csv'.format(age_type, w_range[0], w_range[1]), mode='w')
     fieldnames=['p', 'w', 's', 'base', 'best', 'gain']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
 
-    for w in range(2, time_range//10+1):
+    for w in range(w_range[0] if w_range[0]!=1 else 2, w_range[1]+1):
         for p in [x/100 for x in range(5, 55, 5)]:
             filename = "av_{0}_tr_{1}_ar_{2}_po_{3}_p_{4}_wr_{5}-{6}.csv".format(avg_num, time_range, arrival_type, policy, p, w_range[0], w_range[1])
-            path = './data/'+'av_{0}_tr_{1}_ar_{2}/'.format(avg_num, time_range, arrival_type)+filename
+            path = '/work/LAS/jialiu-lab/zyuan/AoI/data/'+'av_{0}_tr_{1}_ar_{2}/'.format(avg_num, time_range, arrival_type)+filename
             df = pd.read_csv(path, header=0)
             fig = plt.figure() 
             ax = fig.add_subplot(1,1,1)
             plot_s_given_pw(df, p, w, age_type, ax, img_path, writer)
+            csv_file.flush()
             plt.close(fig)
+    
+    print "Done!"
 
 if __name__ == '__main__':
     main()
