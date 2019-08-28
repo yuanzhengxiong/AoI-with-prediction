0594c9c822030e020fbe83a271352f732565f71c
jasonyuan <jasonyuan97@outlook.com>
2019-08-28 14:25:01 -0500

    modified function get_args

--- funcs.py
+++ funcs.py
@@ -1,35 +1,36 @@
-from __future__ import division
-import sys, getopt
+import sys
+import argparse
 
-def parse_args():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hw:t:a:l:p:s:", ["window_size=", "time_range=", "arrival_type=", "lambda=","policy=", "step_size="])
-    except getopt.GetoptError:
-        print 'python main.py -w <window_size> -t <time_range> -a <arrival_type> -l <lambda> -p <policy> -s <step_size>' 
-        sys.exit(2)
-    window_size = ''
-    time_range = ''
-    arrival_type = ''
-    lam = ''
-    policy = ''
-    step_size=""
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'python main.py -w <window_size> -t <time_range> -a <arrival_type> -l <lambda> -p <policy> -s <step_size>' 
-            sys.exit()
-        elif opt in ('-w', '--window_size'):
-            window_size = int(arg)
-        elif opt in ('-t', '--time_range'):
-            time_range = int(arg)
-        elif opt in ('-a', '--arrival_type'):
-            arrival_type = arg
-        elif opt in ('-l', '--lambda'):
-            lam = float(arg)
-        elif opt in ('-p', '--policy'):
-            policy = arg
-        elif opt in ('-s', '--step_size'):
-            step_size = int(arg)
-    return window_size, time_range, arrival_type, lam, policy, step_size
+def get_args():
+    policy = 'equal_spreading'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', type=float)
+    parser.add_argument('-p1', type=float)
+    parser.add_argument('-p2', type=float)
+    parser.add_argument('-w', type=int)
+    parser.add_argument('-t', type=int)
+    parser.add_argument('-a')
+    parser.add_argument('-av', type=int)
+    parser.add_argument('-sew', type=int)
+
+    args = parser.parse_args()
+    w = args.w
+    time_range = args.t
+    arrival_type = args.a
+    avg_num = args.av
+    sew = args.sew
+
+    if(arrival_type=='Bernoulli'):
+        p = args.p
+        return [arrival_type, p, w, time_range, avg_num, policy, sew]
+    elif(arrival_type=='Markovian'):
+        p1 = args.p1
+        p2 = args.p2
+        return [arrival_type, p1, p2, w, time_range, avg_num, policy, sew]
+    else:
+        print("No such arrival type.")
+        sys.exit(-1)
 
 def get_reverse(seq):
     rev = [x for x in seq]
